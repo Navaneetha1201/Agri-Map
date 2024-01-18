349f6e7d7fdd67426d2efd9ac4f2adf95073cc5e
Navaneetha.M <67795564+Navaneetha1201@users.noreply.github.com>
2024-01-18 16:23:36 +0530

    Create mapdata.js

---
+++ mapdata.js
@@ -0,0 +1,345 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   
+		width: "responsive", //or 'responsive'
+    background_color: "#E3F9A6",
+    background_transparent: "yes",
+    border_color: "#F5FFFA",
+    pop_ups: "detect",
+    
+		
+		state_description: "State description",
+    state_color: "#0000FF",
+    state_hover_color: "#3B729F",
+    
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+	
+		location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+		//Label defaults
+		label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+		//Zoom settings
+		zoom: "yes",
+    back_image: "yes",
+    initial_back: "yes",
+    initial_zoom: "-1",
+    initial_zoom_solo: "yes",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+		//Popup settings
+		popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+		div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "START"
+  },
+  state_specific: {
+    "1": {
+      name: "Andaman And Nicobar",
+      description: "ISLAND",
+      color: "#3EB489",
+      hover_color: "default",
+      url: "default"
+    },
+    "2": {
+      name: "Andhra Pradesh",
+      description: "Andhra Pradesh is an exporter of several agricultural products. Rice, Cotton, Sugarcane, Chilli pepper, mango, and tobacco are the local crops. ",
+      color: "#006400",
+      hover_color: "default",
+      url: "http://andhrapradesh.glitch.me/"
+    },
+    "3": {
+      name: "Arunachal Pradesh",
+      description: "Arunachal Pradesh is an Agrarian State where more than 70 percent population is dependent on agriculture for their livelihood.",
+      color: "#4E9258",
+      hover_color: "default",
+      url: "http://arunachalpradesh.glitch.me/"
+    },
+    "4": {
+      name: "Assam",
+      description: "Assam is India's gateway to northeast and acts as a vital link for trade with Southeast Asian countries. The state is known for its tea, petroleum resources, muga silk and biodiversity.",
+      color: "#006400",
+      hover_color: "default",
+      url: "http://assam.glitch.me/"
+    },
+    "5": {
+      name: "Bihar",
+      description: "Agriculture is the backbone of Bihars economy 77% of workforce and generating nearly 24.84% of the State Domestic Product. ",
+      color: "#006400",
+      hover_color: "default",
+      url: "http://bihar1.glitch.me/"
+    },
+    "6": {
+      name: "Chandigarh",
+      description: "Chandigarh is a growing and developing City because of its importance as Union Territory is the Capital of Punjab and Haryana. It depends on out side help for the supply of fruits and vegetable. ",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "7": {
+      name: "Chhattisgarh",
+      description: "Horticulture and animal husbandry engage a major share of the total population of Chhattisgarh. Chhattisgarh is also called the “rice bowl of central India”",
+      color: "#006400",
+      hover_color: "default",
+      url: "http://chhattisgarh.glitch.me/"
+    },
+    "8": {
+      name: "Dadra And Nagar Haveli",
+      description: "Dadra and Nagar Haveli is a district of the Indian union territory of Dadra and Nagar Haveli and Daman and Diu in western India.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "9": {
+      name: "Daman And Diu",
+      description: "ISLAND",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "10": {
+      name: "Delhi",
+      description: "Delhi Government makes effort to help our farmers for their cultivation of crops and here we are providing license for the seeds,fertilizers and Insecticides.",
+      color: "#FFFF00",
+      hover_color: "default",
+      url: "http://delhi.glitch.me/"
+    },
+    "11": {
+      name: "Goa",
+      description: "The total agricultural area is approximately 1400 sq km from which 1200 sq km is owned by the govt and remaining 200 sq km is owned privately. Rice and coconut are the staple produce of Goa.",
+      color: "#E3F9A6",
+      hover_color: "default",
+      url: "http://goa1.glitch.me/"
+    },
+    "12": {
+      name: "Gujarat",
+      description: "More than 50% of the total available land in Gujarat is used for agricultural purposes. The state is divided into 7 sub agro-climatic zones. Gujarat is endowed with abundant natural resources",
+      color: "#254117",
+      hover_color: "default",
+      url: "http://gujarath.glitch.me/"
+    },
+    "13": {
+      name: "Haryana",
+      description: "Known as the “Bread Basket of India”, Haryana has been in the forefront in terms of adoption of latest technologies in agriculture and is also counted as one of the leading states for Agriculture production in the country.",
+      color: "#E3F9A6",
+      hover_color: "default",
+      url: "http://haryana.glitch.me/"
+    },
+    "14": {
+      name: "Himachal Pradesh",
+      description: "Agriculture provides direct employment to 71% of the population in Himachal Pradesh and the sector contributes nearly 30% of the total state domestic product. ",
+      color: "#4E9258",
+      hover_color: "default",
+      url: "https://himachalpradesh.glitch.me"
+    },
+    "16": {
+      name: "Jharkhand",
+      description: " Agriculture is the main stay for the 80% of rural population of the Jharkhand. Agriculture is their employment and primary income generating activity.",
+      color: "#4E9258",
+      hover_color: "default",
+      url: "http://jharkhand1.glitch.me/"
+    },
+    "17": {
+      name: "Karnataka",
+      description: "The main season for agriculture in Karnataka is monsoon as irrigation is done in 26.5 % of the total cropped area. guide to agriculture farming in Karnataka. The agriculture system forms the backbone for the economic development of Karnataka.",
+      color: "#006400",
+      hover_color: "default",
+      url: "http://karnataka1.glitch.me/"
+    },
+    "18": {
+      name: "Kerala",
+      description: "Kerala shares diverse agro-climatic conditions making it fertile for the farming of various crops such as rice, coconut, cloves, coffee and tea. Kerala is known as the spice garden of India",
+      color: "#4E9258",
+      hover_color: "default",
+      url: "http://kerala1.glitch.me/"
+    },
+    "19": {
+      name: "Lakshadweep",
+      description: "ISLAND",
+      color: "ISLAND",
+      hover_color: "default",
+      url: "default"
+    },
+    "20": {
+      name: "Madhya Pradesh",
+      description: "Madhya Pradesh is overhauling its agriculture ecosystem, prioritising technological innovation while centring farmers and their needs",
+      color: "#006400",
+      hover_color: "default",
+      url: "http://madhyapradesh.glitch.me/"
+    },
+    "21": {
+      name: "Maharashtra",
+      description: "Maharashtra state is a leading state in agriculture. Principal crops grown in the State are Rice, Bajra, Jowar, Wheat, Tur, Urad, Mung, Gram, and other Pulses. The State is the main producer of oilseeds.",
+      color: "#254117",
+      hover_color: "default",
+      url: "http://maharashtra.glitch.me/"
+    },
+    "22": {
+      name: "Manipur",
+      description: "Manipur in India is endowed with rich biodiversity and abundant natural resources. Despite inaccessibi-lity, marginality and heterogeneity, the state has made good progress in agriculture and allied sectors.",
+      color: "#E3F9A6",
+      hover_color: "default",
+      url: "http://manipur1.glitch.me/"
+    },
+    "23": {
+      name: "Meghalaya",
+      description: "Meghalaya is basically an agricultural state with about 80% of its population depending entirely on agriculture for their livelihood. Nearly 10% of the geographical area of Meghalaya is under cultivation.",
+      color: "#4E9258",
+      hover_color: "default",
+      url: "http://meghalaya.glitch.me/"
+    },
+    "24": {
+      name: "Mizoram",
+      description: "Rice remains the largest crop grown in Mizoram by gross value of output. Fruits have grown to become the second largest category, followed by condiments and spices.",
+      color: "#4E9258",
+      hover_color: "default",
+      url: "http://mizoram.glitch.me/"
+    },
+    "25": {
+      name: "Nagaland",
+      description: "Over 70% of the population is dependent on agriculture. The main crops are rice, millet, maize and pulses. Cash crops like sugarcane and potato are also becoming popular. ",
+      color: "#4E9258",
+      hover_color: "default",
+      url: "http://nagaland1.glitch.me/"
+    },
+    "26": {
+      name: "Orissa",
+      description: "In Orissa, agriculture acts as the process of crops cultivation and the raising of livestock animals for producing food, feed, fiber, and other desired products. Land of paddy fields.",
+      color: "#E3F9A6",
+      hover_color: "default",
+      url: "http://orissa.glitch.me/"
+    },
+    "27": {
+      name: "Puducherry",
+      description: "Agriculture sector contributes less than 2% of the Gross State Domestic Product (GSDP) of the Government of Puducherry.",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "28": {
+      name: "Punjab",
+      description: "Punjab is one of the most fertile regions on Earth. The region is ideal for growing wheat, rice, sugarcane, fruits and vegetables. Indian Punjab is called the “Granary of India” or India’s bread-basket. It produces 20% of India’s wheat and 9% of India’s rice.",
+      color: "#254117",
+      hover_color: "default",
+      url: "http://punjab.glitch.me/"
+    },
+    "29": {
+      name: "Rajasthan",
+      description: "Rajasthan, with its diverse agro-climatic conditions, is richly endowed in the cultivation of a variety of crops and a strong animal husbandry sector. ",
+      color: "#4E9258",
+      hover_color: "default",
+      url: "https://rajasthan1.glitch.me"
+    },
+    "30": {
+      name: "Sikkim",
+      description: "The agriculture systems practiced in Sikkim are integrated in natures that have evolved through years of experimentation by the farmers. A marginal improvement in the lifestyle of the farmers has been witnessed with the adoption of modern technologies. ",
+      color: "#E3F9A6",
+      hover_color: "default",
+      url: "http://sikkim.glitch.me/"
+    },
+    "31": {
+      name: "Tamil Nadu",
+      description: "Agriculture in Tamilnadu includes the cultivation of crops, animal husbandry, poultry, fish, and forestry activities. 56 percent of the total population of Tamil Nadu is engaged in agriculture. The agricultural sector provides food for men and fodder for livestock.",
+      color: "#006400",
+      hover_color: "default",
+      url: "http://tamilnadu.glitch.me/"
+    },
+    "32": {
+      name: "Tripura",
+      description: "Natural conditions in Tripura are ideal for diverse patterns of cultivation, for the cultivation of cereals, pulses and other food crops, of plantation crops, and of a rich range of agricultural and horticultural crops. ",
+      color: "#E3F9A6",
+      hover_color: "default",
+      url: "http://tripura.glitch.me/"
+    },
+    "33": {
+      name: "Uttar Pradesh",
+      description: "Agriculture is called the backbone of economy of Uttar Pradesh. Uttar Pradesh is the top producer of food grain in Uttar Pradesh. Agriculture provides around 59% employment to the residents of the state.",
+      color: "#254117",
+      hover_color: "default",
+      url: "http://uttarpradsh.glitch.me/"
+    },
+    "34": {
+      name: "Uttaranchal",
+      description: "Like most of India, agriculture is one of the most significant sectors of the economy of Uttarakhand. Basmati rice, wheat, soybeans, groundnuts, coarse cereals, pulses, and oil seeds are the most widely grown crops.",
+      color: "#4E9258",
+      hover_color: "default",
+      url: "default"
+    },
+    "35": {
+      name: "West Bengal",
+      description: "West Bengal is predominantly an agrarian State. Comprising of only 2.7% of India's geographical area, it supports nearly 8% of its population. There are 71.23 lakh farm families of whom 96% are small and marginal farmers.",
+      color: "#254117",
+      hover_color: "default",
+      url: "http://westbengal.glitch.me/"
+    },
+    "36": {
+      name: "Jammu And Kashmir",
+      description: "The Jammu and Kashmir State’s economy is predominately agriculture dependent and nearly 70% of population is directly or indirectly engaged in agricultural and allied occupations. ",
+      color: "#E3F9A6",
+      hover_color: "default",
+      url: "https://jammukashmir.glitch.me"
+    },
+    "37": {
+      name: "Telangana",
+      description: "The Government of Telangana has been giving a high priority to the development of the agriculture sector in the state, given that 5 million farmers depend on it and that the sector contributes to about 15% of the GSDP.",
+      color: "#E3F9A6",
+      hover_color: "default",
+      url: "http://telangana.glitch.me/"
+    },
+    "38": {
+      name: "Ladakh",
+      description: "In Ladakh, 33% of the total land is cultivated under agricultural activities with production of 100,000 quintal per annum of food grains, which include key crops such as wheat, barley and rice produced across the Indus.",
+      color: "#E3F9A6",
+      hover_color: "default",
+  
+      url: "http://ladakh.glitch.me/"
+    },
+    standard: {
+      color: "#cecece",
+      inactive: "yes"
+    }
+  },
+  locations: {
+    "0": {
+      lat: 18.987807,
+      lng: 72.836447,
+      name: "Mumbai"
+    }
+  },
+  labels: {
+      
+          }
+};
